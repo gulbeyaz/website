9de50346108e38dcb5302f1d4c4ff1dd07f023c2
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2024-01-21 21:50:21 +0300

    chore: add head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui','@pinia/nuxt',],
-  pinia: {
-    storesDirs: ['./stores/**'],
-  },
-})
+	app: {
+		head: {
+			title: 'Gulbeyaz',
+			htmlAttrs: {
+				lang: 'en',
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ hid: 'description', name: 'description', content: '' },
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+
+	devtools: { enabled: true },
+	modules: ['@nuxt/ui', '@pinia/nuxt'],
+	pinia: {
+		storesDirs: ['./stores/**'],
+	},
+});
